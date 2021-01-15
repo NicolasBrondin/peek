d6783cef605c1be9696a9b282e97366b6bf05dac
Nicolas Brondin-Bernard <nicolas@brondin.com>
2021-01-15 01:29:38 +0100

    [UX] CLI - Added newline after questions

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ rl.on("close", function() {
   let peek = new ScreenshotManager();
   await peek.init();
   console.log("Peek is a tool to generate screenshot of a website using custom options !");
-  let domainName = await rl.questionAsync("Domain name ? (no trailing slashes)");
+  let domainName = await rl.questionAsync("Domain name ? (no trailing slashes) \n");
   let urls = [];
   let url = "";
   do {
-    url = await rl.questionAsync("Which path do you want to save as screenshot (currently "+urls.length+"), leave empty to go next step");
+    url = await rl.questionAsync("Which path do you want to save as screenshot (currently "+urls.length+"), leave empty to go next step \n");
     if(url){
         urls.push(url);
     }
   } while(urls.length === 0 || url)
-  let width = await rl.questionAsync("Screen width (in px) ?");
-  let height = await rl.questionAsync("Screen height (in px) ?");
+  let width = await rl.questionAsync("Screen width (in px) ? \n");
+  let height = await rl.questionAsync("Screen height (in px) ? \n");
   await peek.generate(domainName, urls, parseInt(width), parseInt(height), false);
   await peek.close();
   rl.close();
